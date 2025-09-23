4a9721cb2ff5f64251904a911e460f69ce9e6ceb
Dias <andremssdias@hotmail.com>
2025-09-23 14:36:13 +0200

    comment unused imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './styles/scss/main.scss'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
