f43e3a24103da1be59602ea0e320bd6ab750e1dc
Dias <andremssdias@hotmail.com>
2025-09-23 22:50:17 +0200

    feat: faq, cta, footer

--- src/main.js
+++ src/main.js
@@ -2,22 +2,15 @@ import './styles/scss/main.scss'
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+document.querySelectorAll(".faq__item").forEach(function (cardElement) {
+    cardElement.addEventListener("click", displayCardParagraph);
+})
 
-// setupCounter(document.querySelector('#counter'))
+function displayCardParagraph() {
+    const btn = this.querySelector(".faq__question");
+    const isExpanded = btn.getAttribute("aria-expanded") === "true";
+    btn.setAttribute("aria-expanded", !isExpanded);
+
+    const paragraph = this.querySelector(".faq__answer");
+    paragraph.toggleAttribute("hidden");
+}
